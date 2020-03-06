24925debe1a6507941af93b607bca53f26f9a71e
jojoengha <wosd1233@gmail.com>
2020-03-06 21:55:45 +0900

    main text bg working...

--- code/script/main.js
+++ code/script/main.js
@@ -3,11 +3,23 @@ $(function () {
     // TODO : <Simple>Code Simplification
     // TODO : button to edge
     // TODO : PHP String binding
+
     var main = classObject("main"),
         about = classObject("about"),
         work = classObject("work"),
         contact = classObject("contact");
 
+    // Logo background text size control
+    var elm = $(".container_main .contents"),
+        bg = elm.find(".bg > img"),
+        text = elm.children(".stationery");
+    
+    text.css({width : bg.width(), height : bg.height()});   // text bg size init
+    $(window).resize(function(){
+        text.css({width : bg.width(), height : bg.height()}); 
+    })
+
+    // Menu text click event
     var moveTop = 0;
     main.click(function () {
         var page = $(".container_main");
